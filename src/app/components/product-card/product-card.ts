e26fae6dc2cde9c264a457c5101e7f0b6fdabf95
Prem Kalekar <premkalekar8@gmail.com>
2026-07-01 21:37:18 +0530

    Move Product to Product card component; Add some product details

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,15 @@
+import { Component, input, output } from '@angular/core';
+import { Product } from '../../models/product';
+import { MatAnchor } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [MatAnchor, MatIcon],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.scss',
+})
+export class ProductCard {
+  product = input.required<Product>();
+  addToCartClicked = output<Product>();
+}
--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -1,10 +1,11 @@
 import { Component, computed, inject, input, signal, Signal } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
+import { ProductCard } from '../../components/product-card/product-card';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
@@ -12,13 +13,16 @@ export class ProductsGrid {
   private readonly _productService = inject(ProductService);
   public category = input<string>('all');
   public products = signal<Product[]>([]);
-  public filteredProducts = computed(() =>
-    this.products().filter(
+  public filteredProducts = computed(() => {
+    if (this.category().toLocaleLowerCase() === 'all') return this.products();
+    return this.products().filter(
       (p) => p.category.toLocaleLowerCase() === this.category().toLocaleLowerCase(),
-    ),
-  );
+    );
+  });
 
   constructor() {
     this.products.set(this._productService.getProducts());
   }
+
+  public addToCart(event: Product) {}
 }
